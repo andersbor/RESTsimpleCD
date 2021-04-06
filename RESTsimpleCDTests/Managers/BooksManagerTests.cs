f97cadbf72be9484830a98bb33c305dfa9b6b932
Anders Børjesson <anbo@zealand.dk>
2021-04-06 17:00:38 +0200

    Test added

---
+++ RESTsimpleCDTests/Managers/BooksManagerTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTsimpleCD.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using RESTsimpleCD.Models;
+
+namespace RESTsimpleCD.Managers.Tests
+{
+    [TestClass()]
+    public class BooksManagerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            BooksManager manager = new BooksManager();
+            List<Book> all = manager.GetAll();
+            Assert.AreEqual(2, all.Count);
+
+        }
+    }
+}
