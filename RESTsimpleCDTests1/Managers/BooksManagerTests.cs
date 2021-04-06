466e9e922184da5ffd098ce708e754ac0f34ac54
Anders Børjesson <anbo@zealand.dk>
2021-04-06 17:11:54 +0200

    test added

---
+++ RESTsimpleCDTests1/Managers/BooksManagerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RESTsimpleCD.Managers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTsimpleCD.Managers.Tests
+{
+    [TestClass()]
+    public class BooksManagerTests
+    {
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
