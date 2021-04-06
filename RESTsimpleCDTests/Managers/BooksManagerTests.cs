8f1fa0b1fa3755800923998ff3c7040eca36adb4
Anders Børjesson <anbo@zealand.dk>
2021-04-06 17:08:47 +0200

    failing test

--- RESTsimpleCDTests/Managers/BooksManagerTests.cs
+++ RESTsimpleCDTests/Managers/BooksManagerTests.cs
@@ -15,7 +15,7 @@ namespace RESTsimpleCD.Managers.Tests
         {
             BooksManager manager = new BooksManager();
             List<Book> all = manager.GetAll();
-            Assert.AreEqual(3, all.Count);
+            Assert.AreEqual(30, all.Count);
 
             Book book = manager.GetById(1);
             Assert.AreEqual("Some title", book.Title);
